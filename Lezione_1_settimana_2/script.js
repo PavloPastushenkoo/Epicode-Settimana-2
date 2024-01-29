3f53bfce0d8ca43e4a9330451d5843caef44f2ed
PavloPastushenkoo <156015193+PavloPastushenkoo@users.noreply.github.com>
2024-01-29 17:44:13 +0100

    Settimana 2 Epicode

---
+++ Lezione_1_settimana_2/script.js
@@ -0,0 +1,19 @@
+let my_Name = 'Pavlo'
+
+console.log(12 + 20)
+
+let x = 12
+
+my_Name = 'Pastushenko'
+
+let subtraction = 4 - x
+console.log(subtraction)
+
+
+let name_1 = 'alessio'
+let name_2 = 'Alessio'
+console.log('name1 e name2 sono uguali:', name_1 === name_2)
+console.log(
+  'name_1 e name_2 sono identici?',
+  name_1.toLowerCase() === name_2.toLowerCase()
+)
